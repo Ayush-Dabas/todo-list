bb000ca82895297b1552e81da9b39f4b54a623fd
Ayush-Dabas <aydbs23@gmail.com>
2023-10-18 19:20:20 +0530

    added heading

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Heading = () => {
-  return <div>Heading</div>;
+  return (
+    <div>
+      <h1 className="flex justify-center items-center text-[50px] text-gray-100 bg-blue-900 py-10">
+        To do list
+      </h1>
+    </div>
+  );
 };
 
 export default Heading;
